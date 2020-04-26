c5386da438f65442287bb06f6c98c946971202ae
Aliaksandr Aksionav <a.aliaksandr@tut.by>
2020-04-26 12:17:20 +0300

    fix rounding

--- lesson2/src/com/basejava/lesson2/Task5.java
+++ lesson2/src/com/basejava/lesson2/Task5.java
@@ -5,7 +5,17 @@ public class Task5 {
 
         int[] myArray = new int[]{11, 2, 3, 11, 5, 6, 7, 8, 9, 12};
 
-        for (int i = myArray.length - 1; i >= 0; i--) {
+        for (int i = 0; i < myArray.length; i++) {
+        }
+        int n = myArray.length;
+        for (int i = 0; i < n/2; i++) {
+            int a;
+            a = myArray[n-i-1];
+            myArray[n-i-1] = myArray[i];
+            myArray[i] = a;
+
+        }
+        for (int i = 0; i < myArray.length ; i++) {
             System.out.print(myArray[i] + " ");
         }
     }
