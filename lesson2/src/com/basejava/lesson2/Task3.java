a71e164b1f08363c61bd1949080a9de11bebcc53
Aliaksandr Aksionav <a.aliaksandr@tut.by>
2020-05-06 10:08:50 +0300

    fix rounding

--- lesson2/src/com/basejava/lesson2/Task3.java
+++ lesson2/src/com/basejava/lesson2/Task3.java
@@ -4,21 +4,20 @@ public class Task3 {
     private static final int ERROR_CODE = -999; // Число не входит в диапазон от 1 до 100
 
     public static void main(String[] args) {
-        int num = numDiv(10);
+        int num = numDiv(99);
         System.out.println(num);
     }
 
     public static int numDiv(int num) {
-        int result = 0;
         if (num > 100) {
             return ERROR_CODE;
         }
-        for (int i = 1; i < num; i++) {
-            if (num % i == 0) {
-                result = i;
-           }
+        for (int i = num; i > 0; i--) {
+            if(i != num && i != 1 && num % i == 0){
+                return i;
+            }
         }
-        return result;
+        return 0;
     }
 }
 
