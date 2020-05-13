70b941f8f0255156dd4e3bf5107951745099363a
Aliaksandr Aksionav <a.aliaksandr@tut.by>
2020-05-13 13:52:16 +0300

    fix rounding

--- lesson6/src/com/basjava/lesson6/conversion/ScannerUtil.java
+++ lesson6/src/com/basjava/lesson6/conversion/ScannerUtil.java
@@ -5,23 +5,27 @@ import java.util.Scanner;
 public class ScannerUtil {
     public static Scanner scanner = new Scanner(System.in);
 
-    public static double getDegree(){
+    public static double getDegree() {
         double degrees = scanner.nextDouble();
         return degrees;
     }
 
     public static double getConverter(double degree) {
+
+        DegreeScale degreeScale = null;
+
         int operation = scanner.nextInt();
         scanner.nextLine();
-        switch (operation){
+        switch (operation) {
             case 1:
-                DegreeScale degreeScale = new CelcToFar(degree);
-                return degreeScale.converter();
+                degreeScale = new CelcToFar(degree);
+                break;
             case 2:
-                DegreeScale degreeScale1 = new CelcToKelv(degree);
-                return degreeScale1.converter();
+                degreeScale = new CelcToKelv(degree);
+                break;
             default:
                 throw new IllegalStateException("Unexpected value: " + operation);
         }
+        return degreeScale.converter();
     }
 }
